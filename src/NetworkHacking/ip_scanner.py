6294998df1a76c3bf544f75b503fd8e8cd6d4cfe
Bruno López Barcia <brunolopar46@gmail.com>
2023-04-19 13:48:42 +0200

    Añadido escaner de hosts en Python

---
+++ src/NetworkHacking/ip_scanner.py
@@ -0,0 +1,26 @@
+import argparse
+import networkscan
+
+parse = argparse.ArgumentParser()
+parse.add_argument("-n", "--network", help="Network IP", required=True)
+parse.add_argument("-o",  "--output", help="Output file", required=False)
+parse.add_argument("-v",  "--verbose", help="Verbose", required=False, type=bool)
+args = parse.parse_args()
+
+def NetWorkScan_module(network):
+
+    scan = networkscan.Networkscan(network)
+    scan.run()
+
+    print("La red " + network + " ha sido escaneada")
+    if (args.output) == None:
+        for i in scan.list_of_hosts_found:
+            print("La IP: " + i + " esta disponible")
+    else:
+        for i in scan.list_of_hosts_found:
+            with open(args.output, "a") as f:
+                f.write("La IP: " + i + " esta disponible" + "\n")
+                f.close()
+
+
+NetWorkScan_module(args.network)

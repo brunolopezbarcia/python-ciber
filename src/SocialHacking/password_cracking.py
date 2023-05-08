f2ebf79202c759c2a2a8bb6cc2d44ae79b138e6b
Bruno López Barcia <brunolopar46@gmail.com>
2023-05-08 19:22:33 +0200

    Añadiendo la funcion de crackear hashes de un archivo
    Signed-off-by: Bruno López Barcia <brunolopar46@gmail.com>

--- src/SocialHacking/password_cracking.py
+++ src/SocialHacking/password_cracking.py
@@ -5,34 +5,39 @@ import sys
 parse = argparse.ArgumentParser()
 parse.add_argument("-p", "--password", help="Pon aqui el hash de la password.", required=False)
 parse.add_argument("-w", "--wordlist", help="Añade aqui el archivo del wordlist", required=True)
-#parse.add_argument("-a", "--archivo", help="Añade el fichero de usuario:contraseña", required=False) TODO Implementar la funcionalidad.
+parse.add_argument("-a", "--archivo", help="Añade el fichero de hashes", required=False) #TODO Implementar la posibilidad de usuario:password
 parse.add_argument("-f", help="Añade el nombre del hash del cifrado(Admitidos: md5, sha1, sha256, sha512, blake2)", required=True)
 args = parse.parse_args()
 
 def check_hash():
-    wordlist = args.wordlist
+    if args.archivo != None:
+        array_hash = open(args.archivo, "r")
+        for hash in array_hash:
+            checker(hash, args.wordlist)
+        array_hash.close()
+    else:
+        hash = args.password
+        checker(hash, args.wordlist)
+
+def checker(hash, wordlist):
     word = open(wordlist, "r")
-    pas = args.password
+    hash_to_check = hash.strip()
     for linea in word:
         contraseña = linea.strip()
-
         if args.f == "sha1":
             hash_dict = hashlib.sha1(contraseña.encode()).hexdigest()
         elif args.f == "md5":
             hash_dict = hashlib.md5(contraseña.encode()).hexdigest()
         elif args.f == "sha256":
-            hash_dict = hashlib.sha256(contraseña.encode).hexdigest()
+            hash_dict = hashlib.sha256(contraseña.encode()).hexdigest()
         elif args.f == "sha512":
             hash_dict = hashlib.sha512(contraseña.encode()).hexdigest()
         elif args.f == "blake2":
             hash_dict = hashlib.blake2b(contraseña.encode()).hexdigest()
 
-
-
-        if pas == hash_dict:
-            print(f'El hash: {args.password} es la palabra: {linea}')
-            sys.exit()
-
+        if hash_to_check == hash_dict:
+            return print(f'El hash: {hash.strip()} es la palabra: {linea}')
+            break
     print("No esta en el wordlist, prueba con otro.")
     word.close()
 
